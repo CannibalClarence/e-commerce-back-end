1a8aa52927bff739dae1198c21bd85562193d783
Theo Phillips <phillipstheo88@gmail.com>
2022-08-27 14:22:27 -0700

    finished db, seeds, and routes

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.send("<h1>Wrong Route!</h1>")
+});
+
+module.exports = router;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Term
